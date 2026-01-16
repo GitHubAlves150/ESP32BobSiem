b970e405147bdfc69efe9ee676148e8295b017df
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2026-01-16 16:08:54 -0300

    full comit

---
+++ Entrada digital/EntradasDigitais/main/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+
+
+#define PIN 14
+void app_main(void)
+{
+    gpio_reset_pin(PIN);
+    gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+    bool status=0;
+
+    while (1)
+    {
+        status=!status;
+        gpio_set_level(PIN, status);
+        /*vTaskDelay(1000 / portTICK_PERIOD_MS);*/
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    
+}
