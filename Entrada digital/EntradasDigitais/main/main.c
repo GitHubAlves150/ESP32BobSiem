da6d9c3b24c2ab46684a4d22f6fb9c6ff186a0f2
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2026-01-16 18:06:24 -0300

    full comit

--- Entrada digital/EntradasDigitais/main/main.c
+++ Entrada digital/EntradasDigitais/main/main.c
@@ -6,17 +6,25 @@
 #include "driver/gpio.h"
 
 
-#define PIN 14
+#define PIN 14    //Pino de saida
+#define BT_PIN 25 //Pino digital de entrada
+
 void app_main(void)
 {
     gpio_reset_pin(PIN);
     gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+
+    gpio_reset_pin(BT_PIN);
+    gpio_set_direction(BT_PIN, GPIO_MODE_OUTPUT);
+
+
     bool status=0;
 
     while (1)
     {
-        status=!status;
+        status=gpio_get_level(BT_PIN); //pega o status do btn_pin
         gpio_set_level(PIN, status);
+
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     
