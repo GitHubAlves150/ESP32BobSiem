46d12d5830679ef968bb906ce78dfbf09d416c6c
lucas <58004390+GitHubAlves150@users.noreply.github.com>
2026-01-22 19:20:54 -0300

    full comit

--- BlinkLed33/main/main.c
+++ BlinkLed33/main/main.c
@@ -6,26 +6,19 @@
 #include "driver/gpio.h"
 
 
-#define PIN 14    //Pino de saida
-#define BT_PIN 25 //Pino digital de entrada
-
+#define PIN 33
 void app_main(void)
 {
     gpio_reset_pin(PIN);
     gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
-
-    gpio_reset_pin(BT_PIN);
-    gpio_set_direction(BT_PIN, GPIO_MODE_OUTPUT);
-
-
     bool status=0;
 
     while (1)
     {
-        status=gpio_get_level(BT_PIN); //pega o status do btn_pin
+        status=!status;
         gpio_set_level(PIN, status);
-
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     
 }
+       
